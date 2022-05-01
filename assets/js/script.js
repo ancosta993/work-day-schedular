0735e8cdae6debe8f6f9c0fde57b83931c4ab90e
Anthony Dcosta <adcosta2000@gmail.com>
2022-04-30 22:50:03 -0400

    added time-blocks

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// display current day
+$('#currentDay').text(moment().format('dddd, MMMM Do')) ;
+
+// initiate an array to store data in the localStorage
+var data = [];
+// objects will be appended in this array. 
+// The properties of these objects are: time, text
+
+
