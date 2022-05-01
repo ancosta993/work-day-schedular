97ee17be3094cacf0326604d077d3f35b6a48c64
Anthony Dcosta <adcosta2000@gmail.com>
2022-05-01 14:53:35 -0400

    can add and edit events. The div size fits the content. save button style added

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,8 +29,47 @@ var auditBlockColor = function(index, el){
 
 // Event handler for changing the text content of the time-block
 $('.container').on('click','.time-block', function(){
-   
-})
+   var text = $(this)
+      .text()
+      .trim();
+
+   // create the textarea
+   var textInput = $("<textarea>")
+      .addClass('form-control')
+      .val(text);
+
+   $(this).replaceWith(textInput);
+
+   textInput.trigger('focus');
+});
+
+// Event handler for moving away from the text input
+$('.container').on('blur','textarea', function(){
+   // get the current text input
+   var text = $(this)
+      .val()
+      .trim();
+      
+   // create the div
+   var textDiv = $("<div>")
+      .addClass('col-10 time-block')
+      .text(text);
+
+   // find the time status of it
+   var time = $(this).closest('.row')
+      .attr('id');
+
+   if (moment().format("HH") - time > 0){
+      textDiv.addClass('past');
+   } else if (moment().format("HH") - time === 0){
+      textDiv.addClass('present');
+   } else {
+      textDiv.addClass('future');
+   }
+
+   $(this).replaceWith(textDiv);
+
+});
 
 // check/change the color every half an hour.
 setInterval(auditBlockColor(), 1000 * 1800);
